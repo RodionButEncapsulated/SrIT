5b8491aba75281e3aba7bc61cdc733169ad7680f
__Rodion__ <pablotapias98@gmail.com>
2021-04-20 17:53:22 -0500

    Sr IT v0.01
    Sr IT versión 0.01:
    Primera versión creada del Software para Registro de Información y Tipificación.

---
+++ Programa de tipificacion.py
@@ -0,0 +1,29 @@
+import time
+
+fecha = time.strftime("%d/%m/%Y %H:%M:%S")
+fecha = str(fecha)
+registro={}
+escribir=''
+
+print(fecha)
+print(fecha.split(' '))
+print('')
+
+while True:
+	nombre = input('Nombre: ')
+	documento = input('Documento: ')
+	extension = input('Extensión: ')
+	sede = input('Sede: ')
+	descripcion = input('Descripción: ')
+	caso = input('Número de caso: ')
+	
+	escribir+=f'Caso: #{caso}\nNombre: {nombre}\nDocumento: {documento}\nExtensión: {extension}\nSede: {sede}\nDescripción: {descripcion}\n\n'
+	
+#	with open(f'{fecha}.txt','w') as file:
+#		file.write(escribir)
+#		print('')
+		
+	file = open(f'{fecha}.txt','w')
+	file.write(escribir)
+	print('')
+	file.close
